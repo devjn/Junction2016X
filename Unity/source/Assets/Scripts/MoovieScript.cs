d7cda7f5078afd4a12bcff69355cdfa787780ddd
devjn <Emperor117.jf@gmail.com>
2016-11-27 12:06:38 +0200

    Initial

---
+++ Unity/source/Assets/Scripts/MoovieScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoovieScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+		Handheld.PlayFullScreenMovie("Main.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+        Application.LoadLevel("GameScene");
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
